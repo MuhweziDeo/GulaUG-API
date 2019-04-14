6222432f9886f6a31e09fffa93ab4a025bd81373
muhwezi deo <aggrey256@gmail.com>
2019-04-14 12:41:04 +0300

    feat(social-authentication): implement social authentication for twitter,facebook,google - setup passport for twitter, facebook, google - add service for registering social users - add controller for mananging social authentication - create a general handler - add endpoint

---
+++ src/helpers/auth/twitter.js
@@ -0,0 +1,36 @@
+const TwitterTokenStrategy = require('passport-twitter-token');
+const passport = require('passport');
+const UserService = require('../../services/userService');
+const uuidv1 = require('uuid/v1');
+
+passport.use('twitter-token',new TwitterTokenStrategy({
+    consumerKey: process.env.TWITTER_CONSUMER_KEY ,
+    consumerSecret: process.env.TWITTER_CONSUMER_SECRET
+     
+  }, async(token, tokenSecret, profile, done) => {
+    try {
+    const {_json:{ screen_name, name, profile_image_url },username, emails, 
+            photos, name: { familyName, givenName }
+        } = profile;  
+    
+    const user = emails[0].value ? 
+        await UserService.findUserByEmail(emails[0].value) :
+        await UserService.findUserByEmail(`${screen_name}@twitter.com`);
+
+    if(!user){
+        const newUser = await UserService.registerSocialUser({
+            email: emails[0].value ? emails[0].value :`${screen_name}@twitter.com`,
+            username: username+uuidv1()
+        },{image: profile_image_url, lastName: givenName, firstName: familyName})
+        return done(null,newUser);
+    }
+    return done(null,user);
+
+    } catch (error) {
+        console.log(error);
+    }
+   
+  }
+));
+
+module.exports = passport;
--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -4,11 +4,16 @@ const userLoginValidator = require('../../middleware/validationMiddleware/user/u
 const UserController = require('./userController');
 const passwordResetRequestValidator = require('../../middleware/validationMiddleware/user/passwordRequestValidator');
 const passwordResetConfirmValidator = require('../../middleware/validationMiddleware/user/passwordResetConfirm');
+const googleFacebookTokenValidator = require('../../middleware/validationMiddleware/user/facebookGoogleValidator'); 
+const twitterTokenValidator = require('../../middleware/validationMiddleware/user/twitterTokenValidator');
 const tokenAuthentication = require('../../middleware/auth/tokenAuthentication');
 const profileUpdateValidator = require('../../middleware/validationMiddleware/user/profileUpdateValidator');
 const { multerUploads } = require('../../middleware/multer');
-router = express.Router()
+const googlePassport = require('../../helpers/auth/google');
+const facebookPassport = require('../../helpers/auth/facebook');
+const twitterPassport = require('../../helpers/auth/twitter');
 
+router = express.Router()
 router.post('/signup',userCreateValidator, UserController.signUpUser);
 
 router.put('/verify/:token/',UserController.verifyUser);
@@ -25,5 +30,10 @@ router.get('/profiles', UserController.getProfiles);
 
 router.put('/profile/:username',tokenAuthentication, multerUploads, profileUpdateValidator, UserController.updateProfile)
 
+router.post('/google',googlePassport.authenticate('google-token',{session: false }),UserController.socialAuthenticationHandler);
+
+router.post('/facebook',googleFacebookTokenValidator,facebookPassport.authenticate('facebook-token',{session: false }),UserController.socialAuthenticationHandler);
+
+router.post('/twitter',twitterTokenValidator, twitterPassport.authenticate('twitter-token',{session: false }),UserController.socialAuthenticationHandler);
 
 module.exports = router;
