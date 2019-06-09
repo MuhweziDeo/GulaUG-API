237eecb310a8884e0c6583869a1801c250ce8bc7
muhwezi deo <aggrey256@gmail.com>
2019-06-09 15:16:05 +0300

    setup admin account creation

---
+++ src/helpers/StartupHelper.js
@@ -0,0 +1,23 @@
+import UserService from '../services/userService';
+import AdminService from '../services/AdminService';
+import createProfileHandler from './createProfilehandler';
+require('dotenv').config;
+
+class StartupHelper {
+    static async createSuperAdmin() {
+        if(!process.env.SUPER_ADMIN_EMAIL) {
+            throw new Error('SuperAdmin Email missing in enviroment');
+            
+        }
+        const user = await UserService.findUserByEmail(process.env.SUPER_ADMIN_EMAIL);
+        if (!user) {
+            const admin = await AdminService.createAdminAccount(process.env.SUPER_ADMIN_EMAIL);
+            await createProfileHandler(admin.username);
+            return;
+        }
+        return 'Super admin alreay exists';
+        
+    }
+}
+
+export default StartupHelper;
